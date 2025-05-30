0295cfcb38db905c690a8f9b3eec2519e49c6afa
NicklesTheBrainless <NicoNickles8@gmail.com>
2025-05-31 01:00:27 +0200

    optimized lsd radix sort

--- Project/src/sorting/algorithms/LSD_RadixSort.java
+++ Project/src/sorting/algorithms/LSD_RadixSort.java
@@ -1,41 +1,51 @@
 package sorting.algorithms;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public abstract class LSD_RadixSort {
 
     public static void radixSort(List<int[]> stepsBuffer, int[] arr, int maxBinaryDigits) {
 
+        int[] binaryArr0 = new int[arr.length];
+        int i0;
+
+        int[] binaryArr1 = new int[arr.length];
+        int i1;
+
         for (int b = 0; b < maxBinaryDigits; b++) {
 
-            List<Integer> list0 = new ArrayList<>();
-            List<Integer> list1 = new ArrayList<>();
+            i0 = 0;
+            i1 = 0;
 
             for (int i : arr) {
 
                 int digit = i & (1 << b);
-                if (digit == 0)
-                    list0.add(i);
-                else
-                    list1.add(i);
+
+                if (digit == 0){
+                    binaryArr0[i0] = i;
+                    i0++;
+                } else {
+                    binaryArr1[i1] = i;
+                    i1++;
+                }
             }
 
-            putTogether(stepsBuffer, arr, list0, list1);
+            putTogether(stepsBuffer, arr, binaryArr0, i0, binaryArr1, i1);
         }
     }
 
-    public static void putTogether(List<int[]> stepsBuffer, int[] totalArr, List<Integer> list0, List<Integer> list1) {
+    public static void putTogether(List<int[]> stepsBuffer, int[] totalArr, int[] arr0, int i0, int[] arr1, int i1) {
 
-        for (int i = 0; i < totalArr.length; i++) {
-
-            if (i < list0.size()) {
-                totalArr[i] = list0.get(i);
-                stepsBuffer.add(totalArr.clone());
-            } else {
-                totalArr[i] = list1.get(i - list0.size());
-                stepsBuffer.add(totalArr.clone());
-            }
+        int i = 0;
+        for (int j = 0; j < i0; j++) {
+            totalArr[i] = arr0[j];
+            stepsBuffer.add(totalArr.clone());
+            i++;
+        }
+        for (int j = 0; j < i1; j++) {
+            totalArr[i] = arr1[j];
+            stepsBuffer.add(totalArr.clone());
+            i++;
         }
     }
 
